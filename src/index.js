3d19b622f87f9ca3966ed8f0aa99e25d055d4c28
Maksym Slinchenko <max.slin777@gmail.com>
2020-11-27 15:49:42 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import "./css/styles.css";
-import getTemplate from "./temlates/page-layout.hbs";
-
-// Напиши функцию delay(ms), которая возвращает промис,
-// переходящий в состояние "resolved" через ms миллисекунд.
-// Значением исполнившегося промиса должно быть то кол-во миллисекунд
-// которое передали во время вызова функции delay.
-
-const delay = (ms) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve(ms), ms);
-    reject(`error`);
-  });
-  // Твой код
-};
-
-const logger = (time) => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+import "./js/index1";
+import "./js/index2";
+import "./js/index3";
